ad124d17ebdb3276b9079ac99c7ff70a00e7fcd8
Daniela M <daguilera3220@gmail.com>
2022-06-26 02:17:24 -0600

    style in footer

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -4,8 +4,8 @@ interface Props {
 
 export const Footer = ({ t }: Props) => {
     return (
-        <footer className="relative bg-purple p-6 border-white border-t-4 text-center">
-            <div className="border-2 p-6">
+        <footer className="relative bg-purple p-6 border-white border-t-4 text-center rounded-lg">
+            <div className="border-2 p-6 rounded-lg">
                 <p className="italic font-bold">
                     Daniela & Daniel
                 </p>
