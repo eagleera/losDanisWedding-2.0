7cdbfa4e4e9746a0f8bece0205a3a16c29f14290
Daniela M <daguilera3220@gmail.com>
2022-06-26 17:19:07 -0600

    target fix

--- app/components/registry/index.tsx
+++ app/components/registry/index.tsx
@@ -93,7 +93,7 @@ export const Registry = ({ t }: Props) => {
               <a href="" className="" target="_blank">
                 <div className="w-48 h-36 mb-4 bg-bone rounded-xl flex justify-center items-center hover:bg-white transition relative">
                   <img
-                    className="absolute top-0 left-1/2 -translate-y-1/2 h-full"
+                    className="absolute top-0 left-1/2 -translate-x-1/2 h-full"
                     src="/images/target.png"
                     alt="target wedding registry"
                   />
